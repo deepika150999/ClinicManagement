e619ad657df432a73662d1476abf6f711ac0b88b
deepika150999 <52271664+deepika150999@users.noreply.github.com>
2022-09-26 19:29:21 +0530

    Add files via upload

---
+++ humanresource.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp5.sqltask10
+{
+    internal class humanresource
+    {
+        public void insData(int BusinessEntityID, DateTime date, UniqueId rowguid)
+        {
+            SqlConnection con = new SqlConnection("Data source=DESKTOP-GQJ9GIB; database=AdventureWorks2019; user id=sa; password=p@ssword");
+            SqlCommand cmd = new SqlCommand("sp_person.password", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@BusinessEntityID", SqlDbType.Int).Value =BusinessEntityID;
+            cmd.Parameters.AddWithValue("@modifiedDate", SqlDbType.Date).Value = date;
+            cmd.Parameters.AddWithValue("@rowguid", SqlDbType.UniqueIdentifier).Value = rowguid;
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Console.WriteLine("Inserted");
+
+        }
+        public static void Main()
+        {
+            Dataa1 dataa1 = new Dataa1();
+
+            dataa1.insData( "101",DateTime.Now, "329EACBE-C883-4F48-B8B6-17AA4627EFFF5676");
+        }
+    }
+}
---
+++ personinperson.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp5.sqltask10
+{
+    internal class personinperson
+    {
+        public void ShowData()
+        {
+            SqlConnection con = new SqlConnection("Data source=DESKTOP-GQJ9GIB; database=AdventureWorks2019; user id=sa; password=p@ssword");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select BusinessEntityID from Person.password", con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader[0].ToString());
+            }
+            con.Close();
+        }
+        public static void Main()
+        {
+            personinperson dataa1 = new personinperson();
+            dataa1.ShowData();
+        }
+
+    }
+}
+
