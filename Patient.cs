a2a65da5e412bca05c8b1b063cdae2c973c10462
deepika150999 <52271664+deepika150999@users.noreply.github.com>
2022-09-29 17:27:00 +0530

    Add files via upload

---
+++ Patient.cs
@@ -0,0 +1,9 @@
+﻿namespace PatientApp.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Disease { get; set; }
+    }
+}
---
+++ PatientController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PatientApp.Models;
+
+namespace PatientApp.Controllers
+{
+    public class PatientController : Controller
+    {
+        public IActionResult Getpatient()
+        {
+            Patient pat = new Patient()
+            {
+                Id = 101,
+                Name = "Mehna",
+                Disease = "Viral Flu "
+            };
+            return View("Patient", pat);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
