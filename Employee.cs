0f2101a4aa76f3b67025d50de30ebdb2e0b68eaf
deepika150999 <52271664+deepika150999@users.noreply.github.com>
2022-10-03 21:24:57 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeApp.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string FirstName { get; set; }
+        public  string LastName { get; set; }   
+        public string Gender { get; set; }
+
+        public static List<Employee> GetEmployeeData()
+        {
+            List<Employee> emp = new()
+        {
+            new Employee(){EmpId=101,FirstName="Arun",LastName="Kumaresan",Gender="male" },
+            new Employee(){EmpId=102,FirstName="Dharani",LastName="ArunKumaresan",Gender="Female"},
+            new Employee(){EmpId=103,FirstName="Satheesh",LastName="Kumaresan",Gender="Male"},
+        new Employee(){EmpId=104,FirstName="Preethi",LastName="SatheeshKumaresan",Gender="Female"}
+            };
+            return emp;
+        }
+        public static Employee AddEmployeeData()
+        {
+            Employee emp1 = new();
+            return emp1;
+
+        }
+        public static Employee Getsinglempdata()
+        {
+            Employee empl = new Employee()
+            {
+                EmpId = 105,
+                FirstName = "Deepika",
+                LastName = "Kumaresan",
+                Gender = "Female"
+            };
+            return empl;
+        }
+    }
+    
+
+}
+    
+
---
+++ EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿ using Microsoft.AspNetCore.Mvc;
+using EmployeeApp.Models;
+
+namespace EmployeeApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult EmployeeData()
+        {
+        List<Employee> employees = Employee.GetEmployeeData();
+            return View(employees);
+        }
+        public IActionResult AddEmployeeData() 
+        {
+            Employee employe =new Employee();
+            return View(employe);
+        }
+        public IActionResult Getsinglempdata()
+        {
+            Employee empl = new Employee()
+            {
+                EmpId = 105,
+                FirstName = "Deepika",
+                LastName = "Kumaresan",
+                Gender = "Female"
+            };
+            return View(empl);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
